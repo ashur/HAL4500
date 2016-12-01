75dd59e12ae1214b0a13384b02dfbeb89a3066d3
Ashur Cabrera <ashur@panic.com>
2016-11-30 17:54:06 -0800

    switched to Wayback-only parsing

--- lib/commands/parse.php
+++ lib/commands/parse.php
@@ -6,38 +6,40 @@ use Cranberry\Core\JSON;
 
 /**
  * @command		parse
- * @desc			Parse the DF feed
+ * @desc			Fetch and parse feeds
  * @usage			parse <url> [<datestamp>]
  */
-$command = new Command\Command( 'parse', "Parse the DF feed", function( $url, $datestamp=null )
+$command = new Command\Command( 'parse', "Fetch and parse feeds", function( $url )
 {
-	/*
-	 * Cache
-	 */
-	$timeLastChecked = $this->cookies->get( 'feed', 'lastChecked' );
-	if( !is_null( $timeLastChecked ) )
+	$year = 2016;
+	$month = 11;
+	$day = 1;
+
+	while( $year >= 2006 )
 	{
-		$cacheLifetime = 60 * 60 * 12;
-		$cacheLifetime = 30;
-		$timeNextCheck = $timeLastChecked + $cacheLifetime;
-		$dateNextCheck = date( 'H:i', $timeNextCheck );
+		$datestamp = sprintf( '%s%02s%02s', $year, $month, $day );
 
-		if( time() < $timeNextCheck )
-		{
-			$this->output->line( "Too soon. Check again at {$dateNextCheck}" );
-			return;
-		}
-	}
-
-	$this->cookies->set( 'feed', 'lastChecked', time() );
-
-	$feedURL = $url;
-	$snapshotsFile = $this->dataDirectory->child( 'snapshots.json' );
-
-	if( $this->getOptionValue( 'use-archive' ) == true )
-	{
 		/*
-	 	 * Wayback Machine
+		 * Cache
+		 */
+		$timeLastChecked = $this->cookies->get( 'feed', 'lastChecked' );
+		if( !is_null( $timeLastChecked ) )
+		{
+			$cacheLifetime = 30;
+			$timeNextCheck = $timeLastChecked + $cacheLifetime;
+
+			if( time() < $timeNextCheck )
+			{
+				echo ( "! Too soon. Check again in " . ($timeNextCheck - time()) . " seconds" ) . PHP_EOL;
+				sleep( 10 );
+				continue;
+			}
+		}
+
+		$this->cookies->set( 'feed', 'lastChecked', time() );
+
+		/*
+			 * Wayback Machine
 		 */
 		$waybackURL  = "http://archive.org/wayback/available?url={$url}";
 		$waybackURL .= empty( $datestamp ) ? '' : "&timestamp={$datestamp}";
@@ -53,10 +55,11 @@ $command = new Command\Command( 'parse', "Parse the DF feed", function( $url, $d
 		$snapshotInfo = $waybackData['archived_snapshots']['closest'];
 		if( $snapshotInfo['available'] != 1 )
 		{
-			$this->output->line( 'Closest snapshot is unavailable' );
+			echo ( '! Closest snapshot is unavailable' ) . PHP_EOL;
 			return;
 		}
 
+		$snapshotsFile = $this->dataDirectory->child( 'snapshots.json' );
 		if( !$snapshotsFile->exists() )
 		{
 			$snapshotsFile->putContents( '[]' );
@@ -65,90 +68,108 @@ $command = new Command\Command( 'parse', "Parse the DF feed", function( $url, $d
 		$snapshotsData = JSON::decode( $snapshotsFile->getContents() );
 		if( in_array( $snapshotInfo['url'], $snapshotsData ) )
 		{
-			$this->output->line( 'Closest snapshot already recorded' );
+			echo ( '! Closest snapshot already recorded' ) . PHP_EOL;
 		}
 
 		$feedURL = $snapshotInfo['url'];
-	}
 
-	/*
- 	 * Feed
-	 */
+		/*
+			 * Feed
+		 */
 
-	echo "Fetching feed '{$feedURL}'...";
-	$feedContents = file_get_contents( $feedURL );
-	echo ' done.' . PHP_EOL;
+		echo "> Fetching feed '{$feedURL}'...";
+		$feedContents = file_get_contents( $feedURL );
+		echo ' done.' . PHP_EOL;
 
-	$snapshotsData[] = $feedURL;
-	$snapshotsJSON = JSON::encode( $snapshotsData );
-	$snapshotsFile->putContents( $snapshotsJSON );
+		$snapshotsData[] = $feedURL;
+		$snapshotsJSON = JSON::encode( $snapshotsData );
+		$snapshotsFile->putContents( $snapshotsJSON );
 
-	/*
- 	 * Parse
-	 */
-	$pattern = '/<title>([^<]*)/i';
-	preg_match_all( $pattern, $feedContents, $matches );
+		/*
+			 * Parse
+		 */
+		$pattern = '/<title>([^<]*)/i';
+		preg_match_all( $pattern, $feedContents, $matches );
 
-	if( count( $matches[1] ) == 0 )
-	{
-		throw new Command\CommandInvokedException( 'Something went wrong while parsing the feed', 1 );
-	}
-
-	$titles = [];
-
-	/* Exclusions */
-	$exclusionsExactMatch = [];
-	$exclusionsContains = [];
-
-	foreach( $matches[1] as $title )
-	{
-		foreach( $exclusionsExactMatch as $exclusion )
+		if( count( $matches[1] ) == 0 )
 		{
-			if( $title == $exclusion )
+			throw new Command\CommandInvokedException( 'Something went wrong while parsing the feed', 1 );
+		}
+
+		$titles = [];
+
+		/* Exclusions */
+		$exclusionsExactMatch = [];
+		$exclusionsContains = [];
+
+		foreach( $matches[1] as $title )
+		{
+			foreach( $exclusionsExactMatch as $exclusion )
 			{
-				continue 2;
+				if( $title == $exclusion )
+				{
+					continue 2;
+				}
+			}
+
+			foreach( $exclusionsContains as $exclusion )
+			{
+				if( substr_count( $title, $exclusion ) > 0 )
+				{
+					continue 2;
+				}
+			}
+
+			$titles[] = $title;
+		}
+
+		/* Update headlines.json */
+		$headlinesFile = $this->dataDirectory->child( 'headlines.json' );
+		if( !$headlinesFile->exists() )
+		{
+			$headlinesFile->putContents( '[]' );
+		}
+
+		$headlinesData = JSON::decode( $headlinesFile->getContents(), true );
+
+		krsort( $titles );
+		$countTitlesAdded = 0;
+
+		foreach( $titles as $title )
+		{
+			if( !in_array( $title, $headlinesData ) )
+			{
+				array_unshift( $headlinesData, $title );
+				$countTitlesAdded++;
 			}
 		}
 
-		foreach( $exclusionsContains as $exclusion )
+		$headlinesJSON = JSON::encode( $headlinesData, JSON_PRETTY_PRINT );
+		$headlinesFile->putContents( $headlinesJSON );
+
+		echo ( "+ Added {$countTitlesAdded} new titles." ) . PHP_EOL . PHP_EOL;
+
+		/* Adjust datestamp */
+		if( $day > 1 )
 		{
-			if( substr_count( $title, $exclusion ) > 0 )
+			$day = $day - 10;
+		}
+		else
+		{
+			/* Roll back to previous month */
+			$day = 21;
+
+			if( $month == 1 )
 			{
-				continue 2;
+				$month = 12;
+				$year--;
+			}
+			else
+			{
+				$month--;
 			}
 		}
-
-		/* Cleanup */
-		$title = str_replace( '★ ', '', $title );
-
-		$titles[] = $title;
 	}
-
-	/* Update headlines.json */
-	$headlinesFile = $this->dataDirectory->child( 'headlines.json' );
-	if( !$headlinesFile->exists() )
-	{
-		$headlinesFile->putContents( '[]' );
-	}
-
-	$headlinesData = JSON::decode( $headlinesFile->getContents(), true );
-
-	krsort( $titles );
-	$countTitlesAdded = 0;
-
-	foreach( $titles as $title )
-	{
-		if( !in_array( $title, $headlinesData ) )
-		{
-			array_unshift( $headlinesData, $title );
-			$countTitlesAdded++;
-		}
-	}
-
-	$headlinesJSON = JSON::encode( $headlinesData );
-	$headlinesFile->putContents( $headlinesJSON );
-
-	$this->output->line( "Added {$countTitlesAdded} new titles." );
 });
 
 $command->registerOption( 'use-archive' );
